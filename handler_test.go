c8c11c350ad0adf68935819dc5f5b3136a2252ca
YanPetrov7 <yanemerald2004@gmail.com>
2023-03-07 17:57:27 +0200

    feat: test for compute method

---
+++ handler_test.go
@@ -0,0 +1,53 @@
+package lab2
+
+import (
+    "bytes"
+    "errors"
+    "io"
+    "strings"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestComputeHandler_Compute(t *testing.T) {
+    type testCase struct {
+        input           string
+        expectedOutput  string
+        expectedError   error
+    }
+
+    testCases := []testCase{
+        {
+            input:          "2 3 +",
+            expectedOutput: "+ 2 3",
+            expectedError:  nil,
+        },
+        {
+            input:          "4 5 * 3 -",
+            expectedOutput: "- * 4 5 3",
+            expectedError:  nil,
+        },
+        {
+            input:          "",
+            expectedOutput: "",
+            expectedError:  io.EOF,
+        },
+        {
+            input:          "invalid input",
+            expectedOutput: "",
+            expectedError:  errors.New("error. Invalid input string"),
+        },
+    }
+
+    for _, tc := range testCases {
+        reader := strings.NewReader(tc.input)
+        writer := &bytes.Buffer{}
+        ch := NewComputeHandler(reader, writer)
+
+        err := ch.Compute()
+
+        assert.Equal(t, tc.expectedError, err)
+        assert.Equal(t, tc.expectedOutput, writer.String())
+    }
+}
